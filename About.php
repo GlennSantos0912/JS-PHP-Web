6c18d2442dcf8abcc01fbfd8431b18d9036c1e57
Glenn Patrick Santos <60724179+GlennSantos0912@users.noreply.github.com>
2020-12-13 20:48:39 +0800

    Update About.php

--- About.php
+++ About.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en-ca">
+<html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Glenn Patrick Santos</title>
-  <link href="Myweb(GlennPatrickSantos).css" rel="stylesheet" type="text/css">
-    <script src="index.js"></script>
+   <link rel="stylesheet" href="Myweb(GlennPatrickSantos).css">
 </head>
 
 <body>
